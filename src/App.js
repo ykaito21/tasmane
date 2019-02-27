b1f6bd6c626a1f162f6bc8df9c19e5db3e6ad992
Yuki Kaito <ykaito21@gmail.com>
2019-02-27 13:47:06 +0900

    basic file structre and build nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>good</h1>
-      </div>
+      <Router>
+        <React.Fragment>
+          <Navbar />
+          <div>
+            <h1>good</h1>
+          </div>
+        </React.Fragment>
+      </Router>
     );
   }
 }
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import SignedInLinks from "./SignedInLinks";
+import SignedOutLinks from "./SignedOutLinks";
+
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="nav-wrapper grey darken-3">
+      <div className="container">
+        <Link to="/" className="brand-logo">
+          Tasmane
+        </Link>
+        <SignedInLinks />
+        <SignedOutLinks />
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
