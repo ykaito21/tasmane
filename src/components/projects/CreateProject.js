06f0fe6d3279ddeb95ac8e4b30df0de5927b14d2
Yuki Kaito <ykaito21@gmail.com>
2019-02-28 18:45:20 +0900

    redirect to protect route

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { createProject } from "../../store/actions/projectActions";
+import { Redirect } from "react-router-dom";
 
 class CreateProject extends Component {
   state = {
@@ -24,6 +25,10 @@ class CreateProject extends Component {
   };
 
   render() {
+    const { auth } = this.props;
+    if (!auth.uid) {
+      return <Redirect to="/signin" />;
+    }
     return (
       <div className="container">
         <form className="white" onSubmit={this.handleSubmit}>
@@ -57,6 +62,12 @@ class CreateProject extends Component {
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    auth: state.firebase.auth
+  };
+};
+
 const mapDispatchToProps = dispatch => {
   return {
     createProject: project => dispatch(createProject(project))
@@ -64,6 +75,6 @@ const mapDispatchToProps = dispatch => {
 };
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(CreateProject);
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,8 +1,8 @@
 const initState = {
   projects: [
-    { id: "1", title: "help me find peach", content: "blah blah blah" },
-    { id: "2", title: "collect all the stars", content: "blah blah blah" },
-    { id: "3", title: "egg hunt with yoshi", content: "blah blah blah" }
+    { id: "1", title: "good man", content: "blah blah blah" },
+    { id: "2", title: "he is nice", content: "blah blah blah" },
+    { id: "3", title: "i am awesome", content: "blah blah blah" }
   ]
 };
 
@@ -13,6 +13,7 @@ const projectReducer = (state = initState, action) => {
       return state;
     case "CREATE_PROJECT_ERROR":
       console.log("create project error", action.err);
+      break;
     default:
       return state;
   }
